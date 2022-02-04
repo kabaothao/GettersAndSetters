5bcc70bf894f3e76d459f2706d26868986fe1e0d
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-04 10:57:50 -0600

    Add project files.

---
+++ GettersAndSetters/Movie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersAndSetters
+{
+    internal class Movie
+    {   //public means these can be access by anyone. any other codes can access to these attributes. public means its open to the world. 
+        public string title;
+        public string director;
+        private string rating;
+        //private string rating; 
+        //what this means is that only code inside of this movie class is going to be able to access the rating. so only code that is contained inside of these
+        //curly brackets over here in this out of this movie class is going to be able to access this 
+
+
+        public Movie(string aTitle, string aDirector, string aRating) //here I created this constructor where I basically pass in a title, a director, a Rating
+                                                                      //and assigned those values.
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; //now this means we're going to set the rating through this setter down here from line 28-46. So this Rating is now calling the Rating below.
+        }
+
+        //you need to define two things
+        public string Rating
+        {
+            get { return rating; } //getter is going to allow us to get the rating attribute.
+                                   //setter is going to allow us to set the rating. 
+                                   //in order to set the rating, we have to go through this if statement. So they can try to pass in whatever rating they want.
+                                   //But if its not equal to G, PG, PG-13, etc, then its just going to get set = to not rating (NR). 
+                                   //So the reason why we need this getter and setter is because we made line 13 private.
+            set { 
+                if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR")
+                    {
+                    rating = value;
+
+                }
+                else
+                {
+                    rating = "NR"; //if its not G. PG, PG-13, R, and NR, then its going to set the rating to NR.
+                }
+            }
+        }
+
+    }
+}
+
+/*
+What is this class Movie for and why?
+Its basically modeling a movie inside of our program. So this is sort of like a templat for the movie data type. 
+So every movie is going to have a title, a director, a Rating.
+
+ */
---
+++ GettersAndSetters/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GettersAndSetters // Note: actual namespace depends on the project name.
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "Dog"); //if you update PG-13 to Dog then you're going to get an invalid value which will be NR. thats because we have to gp through the setter.
+            Movie shref = new Movie("Shrek", "Adam Adamson", "PG");
+
+            Console.WriteLine(avengers.Rating); //so this is the first step to enforce ratings  we need to make the rating field private.
+            //avengers.rating = "Dog"; //this is going to yell at me because this is aprivate attribute. so its not going to let me do that. so that's the point of the setters.
+
+
+            //Example:
+            shref.Rating = "Dog";
+            Console.WriteLine(shref.Rating); 
+
+            Console.ReadLine();
+
+
+            //G, PG, PG-13, R, NR lets say these are all the valid ratings that we can have for a movie.
+            //Also lets say we want to enforce these rules inside of this movie class. So we want all movies in our programs to only be able to have one of these ratings, and it can't have a rating, other than the ones that i specificed down here. 
+            //So how do we enforce that? 
+
+        }
+    }
+}
+
+
+/*
+ Topic: Getters and Setters 
+What is Getters and Setters?
+Getters and Setters are basically two types of methods that we can define inside of our c# classes, which will essentially control the access that people have to the 
+attributes of the classes. And essentially, what this does is it kind of makes your classes more secure. So it kind of allows you to define like, what data is valid for specific attributes
+and what data is invalid. And really getters and setters are extremely useful. And there's a lot of different scenarios where you want to use them. 
+
+ */
+
+
